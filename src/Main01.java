f28f025acb79815c05601f4068874beb0316d104
rinabalakhonova <katia.balahonowa@yandex.ru>
2022-09-08 10:18:27 +0300

    Using Random class for random integer numbers

---
+++ src/Main01.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class Main01 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        // random integer numbers
+
+        // int bound = 100;
+        // for (int i = 0; i < 10; i++) {
+            // System.out.print(random.nextInt(bound) + " ");
+        //}
+
+        // for (int i = 0; 1 < 10; i++) {
+        // System.out.print(random.nextInt() + " ");
+
+    int a = 10;
+    int b = 20;
+    for (int i = 0; i < 10; i++) {
+        System.out.print(random.nextInt( bound b - a + 1) + a + " ");
+
+}
+}
+}
