adba57048839271f8dd13f3ca6684a3c274f4b19
rinabalakhonova <katia.balahonowa@yandex.ru>
2022-09-08 09:32:32 +0300

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
+        System.out.println("Testing GitHub...");
     }
 }
